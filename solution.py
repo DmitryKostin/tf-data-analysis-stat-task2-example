19f3d3971499ecec6d07f8fcf696e2a478bdfd95
Dmitry Kostin <112777722+DmitryKostin@users.noreply.github.com>
2023-04-06 15:57:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,8 +4,5 @@ import numpy as np
 chat_id = 944932368
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
     alpha = 1 - p
-    b_min = np.min(x) / (1 - alpha)
-    b_max = np.max(x) / (1 - alpha)
-    return b_min, b_max
+    return x.mean() - np.sqrt(np.var(x)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)), x.mean() - np.sqrt(np.var(x)) * norm.ppf(alpha / 2) / np.sqrt(len(x))
