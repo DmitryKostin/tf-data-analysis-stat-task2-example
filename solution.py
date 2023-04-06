26d6dfe77db882f858987a562ebad97e9f2b23d4
Dmitry Kostin <112777722+DmitryKostin@users.noreply.github.com>
2023-04-06 15:20:38 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import t
 
 chat_id = 944932368
 
 def solution(p: float, x: np.array) -> tuple:
-    alpha = 1 - p
     n = len(x)
-    x_bar = np.mean(x)
-    s = np.std(x, ddof=1)
-    t_alpha = np.abs(t.ppf(alpha/2, n-1))
-    lower = x_bar - t_alpha*s/np.sqrt(n)
-    upper = x_bar + (1-t_alpha)*s/np.sqrt(n)
-    return lower, upper
+    alpha = 1 - p
+    b_min = np.min(x) / (1 - alpha)
+    b_max = np.max(x) / (1 - alpha)
+    return b_min, b_max
