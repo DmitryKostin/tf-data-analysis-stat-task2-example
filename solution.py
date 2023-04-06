c16825401f5e5a9bc73fa80cedc9f873b33379a3
Dmitry Kostin <112777722+DmitryKostin@users.noreply.github.com>
2023-04-06 14:39:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,15 @@
 import pandas as pd
 import numpy as np
-
-from scipy.stats import norm
-
+from scipy.stats import t
 
 chat_id = 944932368
 
 def solution(p: float, x: np.array) -> tuple:
-    alpha = 1 - p 
+    alpha = 1 - p
     n = len(x)
-    x_bar = np.mean(x) 
+    x_bar = np.mean(x)
     s = np.std(x, ddof=1)
-    z_alpha = np.abs(norm.ppf(alpha/2))
-    lower = x_bar - z_alpha*s/np.sqrt(n)
-    upper = x_bar + (1-z_alpha)*s/np.sqrt(n)
+    t_alpha = np.abs(t.ppf(alpha/2, n-1))
+    lower = x_bar - t_alpha*s/np.sqrt(n)
+    upper = x_bar + (1-t_alpha)*s/np.sqrt(n)
     return lower, upper
